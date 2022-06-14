b43eb4753553aeae908acce54ffb54cdae7402aa
ziqian pan <ziqianpan1998@gmail.com>
2022-06-14 19:58:31 -0400

    linked page to show on App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <h1>Hello World</h1>
+      <Nav />
+      <Home />
+      <About />
     </>
-  )
+  );
 }
 
 export default App;

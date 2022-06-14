ad61c6c64420828559f39d46a5c49a06d2253c6a
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-14 19:58:03 -0400

    About Page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <p>About Project</p>
+    <sections>Ziqian
+      <img src='https://avatars.githubusercontent.com/u/96352580?v=4'/>
+      <cite>https://github.com/ZiqianPan</cite>
+    </sections>
+    <sections>Taji
+      <img src ="https://avatars.githubusercontent.com/u/82912560?v=4"/>
+      <cite>https://github.com/Nique444</cite>
+    </sections>
+    <sections>Kalilah
+      <img src ="https://avatars.githubusercontent.com/u/75289632?v=4"/> 
+      <cite>https://github.com/KalilahClarke</cite>
+    </sections>
+    
+    </>
   )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <>
+    <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png"/>
+    <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png"/>
+    {/* <img src="" alt=""/>
+    <img/>
+    <img/> */}
+    </>
   )
 }
 

02ae65306ec3f9b82923a3f759c7dc56dccc757b
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-15 18:46:32 -0400

    Force Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,14 @@ function Header() {
 
   }
   return (
-    <>
-    <img src ="https://www.freeiconspng.com/thumbs/menu-icon/menu-icon-0.png" alt ="Hamburger DropDown"/>
-    <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="Youtube Logo"/>
+    <div className="Header">
+    <img  className="Youtube" src="https://socalrealtylaw.com/wp-content/uploads/2021/09/R-1.png" alt="Youtube Logo"/>
     <form value ={HandleSubmit}>
        <input placeholder ='Search' type = 'text'/>
       <button>Submit</button> 
 
     </form>
-    </>
+    </div>
   )
 }
 

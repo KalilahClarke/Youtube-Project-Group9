bea596077d3658fe124262a466030b5f425b0d16
T.K <nique@niques-air.myfiosgateway.com>
2022-06-15 19:34:26 -0400

    adding new key

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
     <>
     <img src ="https://www.freeiconspng.com/thumbs/menu-icon/menu-icon-0.png" alt ="Hamburger DropDown"/>
     <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="Youtube Logo"/>
-    <form value ={HandleSubmit}>
+    <form onSubmit={ HandleSubmit }>
        <input placeholder ='Search' type = 'text'/>
       <button>Submit</button> 
 

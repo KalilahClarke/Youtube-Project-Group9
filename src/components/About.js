7e653ccbd3841ae31133ef6f84b774872417b6cd
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-14 20:03:25 -0400

    About Template

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <>
-    <p>About Project</p>
-    <sections>Ziqian
-      <img src='https://avatars.githubusercontent.com/u/96352580?v=4'/>
-      <cite>https://github.com/ZiqianPan</cite>
-    </sections>
-    <sections>Taji
-      <img src ="https://avatars.githubusercontent.com/u/82912560?v=4"/>
-      <cite>https://github.com/Nique444</cite>
-    </sections>
-    <sections>Kalilah
-      <img src ="https://avatars.githubusercontent.com/u/75289632?v=4"/> 
-      <cite>https://github.com/KalilahClarke</cite>
-    </sections>
-    
+      <h1>About Project</h1>
+      <p></p>
+
+      <h2>Creators</h2>
+      <sections>
+        <h3>Ziqian Pan</h3>
+        <img src="https://avatars.githubusercontent.com/u/96352580?v=4" />
+        <cite>https://github.com/ZiqianPan</cite>
+        <p></p>
+      </sections>
+
+      <sections>
+        <h3>Taji Fuller</h3>
+        <img src="https://avatars.githubusercontent.com/u/82912560?v=4" />
+        <cite>https://github.com/Nique444</cite>
+        <p></p>
+      </sections>
+
+      <sections>
+        <h3>Kalilah Clarke</h3>
+        <img src="https://avatars.githubusercontent.com/u/75289632?v=4" />
+        <cite>https://github.com/KalilahClarke</cite>
+        <p></p>
+      </sections>
     </>
-  )
+  );
 }
 
-export default About
+export default About;

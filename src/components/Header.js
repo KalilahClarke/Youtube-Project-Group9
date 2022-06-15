afff424080bd182cf89cfb43e3859366c35b5865
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-15 19:39:17 -0400

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
     <img  className="Youtube" src="https://socalrealtylaw.com/wp-content/uploads/2021/09/R-1.png" alt="Youtube Logo"/>
     <form value ={HandleSubmit}>
        <input placeholder ='Search' type = 'text'/>
-      <button>Submit</button> 
+      <button><img src="https://img.icons8.com/ios-glyphs/30/undefined/search--v1.png" alt="search icon"/></button> 
 
     </form>
     </div>

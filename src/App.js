1db9d605c53ca18e6403c88e1240a72348103c24
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-06-15 09:09:20 -0400

    Cleaned Terminal Warnings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import logo from "./logo.svg";
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import Header from "./components/Header"
 
 import YouTube from 'react-youtube';
 
@@ -13,10 +14,10 @@ function App() {
   return (
     
     <>
-      <YouTube videoId="2g811Eo7K8U"  />;
+      {/* <YouTube videoId="2g811Eo7K8U"  />; */}
     <div>
       <Router>
-  
+        <Header/>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Header.css"
 
 function Header() {
+  const HandleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(e)
+
+  }
   return (
-    <div>Header</div>
+    <>
+    <img src ="https://www.freeiconspng.com/thumbs/menu-icon/menu-icon-0.png" alt ="Hamburger DropDown"/>
+    <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="Youtube Logo"/>
+    <form value ={HandleSubmit}>
+       <input placeholder ='Search' type = 'text'/>
+      <button>Submit</button> 
+
+    </form>
+    </>
   )
 }
 

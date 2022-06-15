0dcc066ad4b86e7932eea4ad45b0af98350f46ac
T.K <nique@niques-air.myfiosgateway.com>
2022-06-14 21:36:05 -0400

    commenting home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ function Nav() {
   return (
     <>
 <ul>
-  {/* <li onClick={ handleClickHome=<Home /> }><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" width="25px"/> Home</Link></li> */}
+  <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" width="25px"/> Home</Link></li>
 
   <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" width="25px"/> About</Link></li>
 </ul>

5e7325f29c54e94ed0d123b253ec361545e44a02
ziqian pan <ziqianpan1998@gmail.com>
2022-06-16 16:33:26 -0400

    refactor nav.js and nav.css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,53 +1,32 @@
 import "./Nav.css";
 import React, { useState } from "react";
-
 // import Home from "./Home";
 // import About from "./About";
 import { Link } from "react-router-dom";
-import { within } from "@testing-library/react";
 
 function Nav() {
 
   const [showNav, setShowNav] = useState(false);
-  const [style, setStyle] = useState("sidenav1");
+  const [style, setStyle] = useState("sidenavOpen");//this might need to be moved once home.js is done.
 
   //https://www.geeksforgeeks.org/changing-css-styling-with-react-onclick-event/
 
 const closeOpenNav = (e) => {
   setShowNav(!showNav)//keeps T/F
   if (showNav) {
-    setStyle("sidenav1");
+    setStyle("sidenavOpen");
   } else {
-    setStyle("sidenav2");
+    setStyle("sidenavClose");
   }
-
-console.log("its changing")
-
-
-//how can I make this change the css of the nav below. 
 }
-
-//function that return/show the nav bar
-//
-const Bar = () => {
-
-
-  return ( 
-  <div>
-  <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
-  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
-  </div>
-  )
-}
-
 
   return (
     <div id="mySidenav" className={style}>
 <ul>
+{/* this top one needs to be in the home.js - not here. */}
   <li onClick={closeOpenNav}><img  className="Hamburger"src ="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-20.jpg" alt ="Hamburger DropDown" width={"100px"}/></li>
-  
-  {/* {showNav ? <Bar/> : null} */}
-  
+
+
   <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
   <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
 </ul>

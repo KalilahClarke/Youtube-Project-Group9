486b7b76950ec35d4c766ce9f688a5dd6c4157e7
ziqian pan <ziqianpan1998@gmail.com>
2022-06-15 22:07:07 -0400

    ugly but working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,37 +4,52 @@ import React, { useState } from "react";
 // import Home from "./Home";
 // import About from "./About";
 import { Link } from "react-router-dom";
+import { within } from "@testing-library/react";
 
 function Nav() {
 
   const [showNav, setShowNav] = useState(false);
+  const [style, setStyle] = useState("sidenav1");
+
+  //https://www.geeksforgeeks.org/changing-css-styling-with-react-onclick-event/
 
 const closeOpenNav = (e) => {
-setShowNav(!showNav)//keeps T/F
+  setShowNav(!showNav)//keeps T/F
+  if (showNav) {
+    setStyle("sidenav1");
+  } else {
+    setStyle("sidenav2");
+  }
+
 console.log("its changing")
+
+
 //how can I make this change the css of the nav below. 
 }
 
 //function that return/show the nav bar
 //
 const Bar = () => {
+
+
   return ( 
-  <>
+  <div>
   <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
   <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
-  </>
+  </div>
   )
 }
 
 
   return (
-    <div id="mySidenav" className="sidenav">
+    <div id="mySidenav" className={style}>
 <ul>
   <li onClick={closeOpenNav}><img  className="Hamburger"src ="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-20.jpg" alt ="Hamburger DropDown" width={"100px"}/></li>
-  {showNav ? <Bar/> : null}
   
-  {/* <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
-  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li> */}
+  {/* {showNav ? <Bar/> : null} */}
+  
+  <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
+  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
 </ul>
     </div>
   )

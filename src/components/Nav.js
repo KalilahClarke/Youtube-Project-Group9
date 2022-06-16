f5218406aac931127a1f7ec16c24fb23deab7125
ziqian pan <ziqianpan1998@gmail.com>
2022-06-15 21:34:38 -0400

    got button to work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,8 +10,20 @@ function Nav() {
   const [showNav, setShowNav] = useState(false);
 
 const closeOpenNav = (e) => {
-setShowNav(!showNav)
+setShowNav(!showNav)//keeps T/F
 console.log("its changing")
+//how can I make this change the css of the nav below. 
+}
+
+//function that return/show the nav bar
+//
+const Bar = () => {
+  return ( 
+  <>
+  <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
+  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
+  </>
+  )
 }
 
 
@@ -19,8 +31,10 @@ console.log("its changing")
     <div id="mySidenav" className="sidenav">
 <ul>
   <li onClick={closeOpenNav}><img  className="Hamburger"src ="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-20.jpg" alt ="Hamburger DropDown" width={"100px"}/></li>
-  <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
-  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
+  {showNav ? <Bar/> : null}
+  
+  {/* <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
+  <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li> */}
 </ul>
     </div>
   )

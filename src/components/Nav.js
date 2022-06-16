29d8d631c9b298883b6eb6c3bc9a1a22db1164df
ziqian pan <ziqianpan1998@gmail.com>
2022-06-15 21:12:11 -0400

    working on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,16 +7,18 @@ import { Link } from "react-router-dom";
 
 function Nav() {
 
+  const [showNav, setShowNav] = useState(false);
 
-const closeOpenNav = () => {
-ajsdijasidad
+const closeOpenNav = (e) => {
+setShowNav(!showNav)
+console.log("its changing")
 }
 
 
   return (
     <div id="mySidenav" className="sidenav">
 <ul>
-  <li><img  className="Hamburger"src ="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-20.jpg" alt ="Hamburger DropDown" width={"100px"}/></li>
+  <li onClick={closeOpenNav}><img  className="Hamburger"src ="https://icon-library.com/images/hamburger-menu-icon-transparent/hamburger-menu-icon-transparent-20.jpg" alt ="Hamburger DropDown" width={"100px"}/></li>
   <li><Link to="/"> <img src="https://img.icons8.com/material-sharp/24/undefined/home-page.png" alt="Home" width="25px"/> Home</Link></li>
   <li><Link to="/about"> <img src="https://img.icons8.com/ios-filled/50/undefined/compass--v1.png" alt="About" width="25px"/> About</Link></li>
 </ul>

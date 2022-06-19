924dbd8b2d23b273f9b8c4b9bc65f2382ad069d3
ziqian pan <ziqianpan1998@gmail.com>
2022-06-19 14:48:55 -0400

    edit/route video and videolist.

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import {Link, useParams } from "react-router-dom"
 
-export default function videoList() {
+export default function VideoList() {
   return (
-    <div>videoList</div>
+    <div><h1>VideoList</h1></div>
   )
 }
+
+
+
+// export default function MovieListing({movie}) {
+//     const {title,description,duration,id, listedIn} = movie;
+//     // console.log(movie);
+
+//     const params = useParams();
+//     // console.log(params);
+
+//   return (
+//     <article className="show">
+//     <h3 class="title">
+//       <Link to={`/movies/${id}`}>{title}</Link>
+//     </h3>
+//     <p className="description">{description}</p>
+//     <aside className="details">
+//       <p><span>Listed Categories:</span> {listedIn}</p>
+//       <p><span>Duration:</span> {duration}</p>
+//     </aside>
+//   </article>
+//   )
+// }

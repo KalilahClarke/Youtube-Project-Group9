c4dd91e3b77c135303987b13da32fb8723892f07
T.K <nique@niques-air.myfiosgateway.com>
2022-06-20 09:24:55 -0400

    fixing home and thumbnail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 // import logo from "./logo.svg";
 import "./App.css";
@@ -13,22 +13,7 @@ import Header from "./components/Header"
 function App() {
   //Do not forget to add useEffect to import//
 
-  let [videos, setVideos] = useState([])
-
-  useEffect(() => {
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
-      )
-    .then ((response) => response.json())
-    .then ((data) => {
-      setVideos(data.items)
-      console.log(data.items)
-    })
-    
-    .catch ((err) => {
-      console.log(err)
-    })
-  })
+  const [videos, setVideos] = useState([])
 
   return (
     
@@ -40,7 +25,7 @@ function App() {
         <Header/>
         <Nav />
         <Routes>
-          <Route path="/" element={<Home videos={ videos }/>} />
+          <Route path="/" element={<Home videos={ videos } setVideos={ setVideos }/>} />
           <Route path="/About" element={<About />} />
         </Routes>
         </div>

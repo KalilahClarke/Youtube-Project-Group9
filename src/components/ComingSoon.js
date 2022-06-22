50b614ee7a04a97d7da8d7f23f517b91ffbe98cc
ziqian pan <ziqianpan1998@gmail.com>
2022-06-22 17:47:05 -0400

    add home button to coming soon page

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import "./ComingSoon.css"
+import { useNavigate } from "react-router-dom";
 
 export default function ComingSoon() {
+  const navigate = useNavigate();
+
+  const handleHome = () => {
+    navigate("/");
+  };
+
   return (  
   <div class="middle">
     <h1>COMING SOON</h1>
     <p>Work in progress</p>
+    <button onClick={handleHome}>Go Back to Home</button>
   </div>
   )
 }
